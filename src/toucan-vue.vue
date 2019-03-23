fc51b20c98f5d65b8936ddb88a46580d446b935e
Vishnu Menon <me@vishnumenon.com>
2019-03-23 12:08:19 -0400

    Initial commit

---
+++ src/toucan-vue.vue
@@ -0,0 +1,73 @@
+<template>
+  <div v-if="embeddedMode" style="width: 100%; height: 100%"></div>
+</template>
+
+<script>
+function waitForToucan(resolve) {
+  if (typeof ToucanAI !== "undefined") {
+    resolve();
+  }
+  setTimeout(() => waitForToucan(resolve), 200);
+}
+
+function loadScript(src, alreadyLoaded) {
+  return new Promise(function(resolve, reject) {
+    if (alreadyLoaded) {
+      waitForToucan(resolve);
+    } else {
+      var tag = document.createElement("script");
+      tag.async = false;
+      tag.src = src;
+      tag.addEventListener("load", resolve);
+      tag.addEventListener("error", reject);
+      document.getElementsByTagName("body")[0].appendChild(tag);
+    }
+  });
+}
+
+export default {
+  name: "ToucanAIChat",
+  props: [
+    "id",
+    "secret",
+    "tryToResume",
+    "messageDelay", // in ms
+    "onProductsSelected", // argument is an array of product objects
+    "onProductClicked", // arguments are Product, Event. Return 'true' if the link should be followed after calling callback
+    "onGoalReached", // arguments are the Product for which the goal was reached
+    "onReady", // Fires when toucan is fully ready/rendered, with 'this' as argument
+    "customStylesheet",
+    "accentColor", // Defaults to "247, 71, 6", Must be a comma-separated RGB triplet string!!
+    "embeddedMode", // When in embedded mode, only the message log renders; no FAB etc
+    "allowInput", // When false, don't show input
+    "loadConvo", // When non-null, load the provided convo ID and resume it
+    "hidePreviews",
+    "alreadyLoaded" // set to true on all but the first-loaded instance on a page
+  ],
+  mounted: function() {
+    loadScript(
+      "https://dev.toucanai.com:91/widget.js",
+      this.alreadyLoaded
+    ).then(() => {
+      let options = Object.assign({}, this.$props);
+      // Delete any undefined keys
+      Object.keys(options).forEach(
+        key => options[key] === undefined && delete options[key]
+      );
+      if (this.embeddedMode) {
+        options.parent = this.$el;
+      }
+      options.onReady = t => {
+        this.toucanInstance = t;
+        if (this.onReady) {
+          this.onReady();
+        }
+      };
+      new ToucanAI(options);
+    });
+  }
+};
+</script>
+
+<style>
+</style>
